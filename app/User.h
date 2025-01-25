5524172731a16092f3faf97e8aca4b37d20dfcb1
k-erlikh <kateerlikh@cmail.carleton.ca>
2025-01-25 16:45:16 -0500

    Temporarily updated function access for testing

--- app/User.h
+++ app/User.h
@@ -13,15 +13,17 @@ using namespace std;
 class User{
 
     public:
-        User(string name);
-        string name;
+        User();
         vector<Bases> bases;
         vector<int> key;
         vector<int> sifted_key;
+        void print_key();
+        void print_bases();
 
     protected:
         void pick_base(int bit_length);
         void sift_key(vector<Bases> base_1, vector<Bases> base_2);
+        
 };
 
 #endif
