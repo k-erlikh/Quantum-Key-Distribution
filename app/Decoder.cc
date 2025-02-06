f902db57a179dd30f4a625ff5a319ef53adf3b31
k-erlikh <kateerlikh@cmail.carleton.ca>
2025-02-06 13:50:41 -0500

    Added decoding function

--- app/Decoder.cc
+++ app/Decoder.cc
@@ -3,9 +3,24 @@
 Decoder::Decoder(){}
 
 void Decoder::decode_message(Channel &channel){
-    
+    for(int i = 0; i < this->bases.size(); i++){
+        int photon = channel.pop();
+        if (bases[i] == BASE_ZERO){
+            if(photon == 0){
+                key.push_back(0);
+            }
+            if(photon == 90){
+                key.push_back(1);
+            }
+        }
+        if (bases[i] == BASE_ONE){
+            if(photon == 45){
+                key.push_back(0);
+            }
+            if (photon == 135){
+                key.push_back(1);
+            }
+        }
+    }
 }
 
-void Decoder::get_encoder_bases(Encoder &encoder){
-    this->bases = encoder.bases;
-}
--- app/Decoder.h
+++ app/Decoder.h
@@ -9,9 +9,6 @@ class Decoder : public User{
 
     public:
         Decoder();
-        void get_encoder_bases(Encoder& encoder);
-        
-    private:
         void decode_message(Channel& channel);
 };
 
