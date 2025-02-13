c9593c9b78a041450531a06c72471e7c2180e36b
k-erlikh <kateerlikh@cmail.carleton.ca>
2025-02-12 22:12:05 -0500

    Updated to handle mismatched bases

--- app/Decoder.cc
+++ app/Decoder.cc
@@ -3,22 +3,29 @@
 Decoder::Decoder(){}
 
 void Decoder::decode_message(Channel &channel){
-    for(int i = 0; i < this->bases.size(); i++){
+    srand(time(0)+1);
+    for(int i = 0; i < bases.size(); i++){
         int photon = channel.pop();
         if (bases[i] == BASE_ZERO){
             if(photon == 0){
                 key.push_back(0);
             }
-            if(photon == 90){
+            else if(photon == 90){
                 key.push_back(1);
+            }
+            else{
+                key.push_back(rand() % 2);
             }
         }
         if (bases[i] == BASE_ONE){
             if(photon == 45){
                 key.push_back(0);
             }
-            if (photon == 135){
+            else if (photon == 135){
                 key.push_back(1);
+            }
+            else{
+                key.push_back(rand() % 2);
             }
         }
     }
